a49c2043b398590d2020ccfac3f39686d692c495
Limm-jk <201602057@cs-cnu.org>
2020-06-08 00:27:38 +0900

    first build
    clone해온 파일을 build하여
    내의 Page에서 정상 작동하는 지
    테스트 하기 위함

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-calculator/precache-manifest.72f01110fbb96038031279d4d11eea00.js"
+  "/react-calculator/precache-manifest.0a7470302dc7de6f932e3b0cdecfaeae.js"
 );
 
 workbox.clientsClaim();
